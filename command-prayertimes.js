458fba8272bc22bbe07828628eecb7c681f476f5
Sarim <sarimyx@proton.me>
2025-10-07 18:40:33 -0600

    use practices endpoint

--- command-prayertimes.js
+++ command-prayertimes.js
@@ -7,56 +7,59 @@ exports.default = new Command()
   .alias("pt")
   .description("Get live prayer times for any part of the world")
   .argument("<string...>", "search")
+  .option("-mm, --midpoint_method", "Use midpoint method for Asr prayer", false)
   .action(async (query, options) => {
     baseLog();
 
     query = query.join(" ");
 
     const fetchURL = new URL(
-      `https://api.wikisubmission.org/prayer-times`
+      `https://practices.wikisubmission.org/prayer-times/${query}`
     );
-    fetchURL.searchParams.append("q", query);
     fetchURL.searchParams.append("highlight", "true");
+    if (options.midpoint_method) {
+      fetchURL.searchParams.append("asr_adjustment", "true");
+    }
 
     const request = await fetch(fetchURL);
     const response = await request.json();
 
-    if (response.results && !response.error) {
+    if (!response.error) {
       logger
         .color("green")
         .bold()
         .underscore()
-        .log(`${response.results.location_string}`);
+        .log(`${response.location_string}`);
 
       logger
         .color("white")
         .log(
-          `${response.results.local_time} (${response.results.local_timezone})\n`
+          `${response.local_time} (${response.local_timezone})\n`
         );
       logger.bgColor("magenta").color("black").bold().log(`Live Status`);
       logger
         .color("yellow")
-        .log(`${formatMarkdown(response.results.status_string)}\n`);
+        .log(`${formatMarkdown(response.status_string)}\n`);
 
       logger.bgColor("magenta").color("black").bold().log(`Schedule`);
-      logger.color("white").log(`Fajr: ${response.results.times.fajr}`);
-      logger.color("white").log(`Dhuhr: ${response.results.times.dhuhr}`);
-      logger.color("white").log(`Asr: ${response.results.times.asr}`);
-      logger.color("white").log(`Maghrib: ${response.results.times.maghrib}`);
-      logger.color("white").log(`Isha: ${response.results.times.isha}`);
-      logger.color("white").log(`Sunrise: ${response.results.times.sunrise}\n`);
+      logger.color("white").log(`Fajr: ${response.times.fajr}`);
+      logger.color("white").log(`Dhuhr: ${response.times.dhuhr}`);
+      logger.color("white").log(`Asr: ${response.times.asr}`);
+      logger.color("white").log(`Maghrib: ${response.times.maghrib}`);
+      logger.color("white").log(`Isha: ${response.times.isha}`);
+      logger.color("white").log(`Sunrise: ${response.times.sunrise}\n`);
 
       logger.bgColor("magenta").color("black").bold().log(`Times Left`);
-      logger.color("white").log(`Fajr: ${response.results.times_left.fajr}`);
-      logger.color("white").log(`Dhuhr: ${response.results.times_left.dhuhr}`);
-      logger.color("white").log(`Asr: ${response.results.times_left.asr}`);
+      logger.color("white").log(`Fajr: ${response.times_left.fajr}`);
+      logger.color("white").log(`Dhuhr: ${response.times_left.dhuhr}`);
+      logger.color("white").log(`Asr: ${response.times_left.asr}`);
       logger
         .color("white")
-        .log(`Maghrib: ${response.results.times_left.maghrib}`);
-      logger.color("white").log(`Isha: ${response.results.times_left.isha}`);
+        .log(`Maghrib: ${response.times_left.maghrib}`);
+      logger.color("white").log(`Isha: ${response.times_left.isha}`);
       logger
         .color("white")
-        .log(`Sunrise: ${response.results.times_left.sunrise}`);
+        .log(`Sunrise: ${response.times_left.sunrise}`);
     } else if (!response.error) {
       logger.color("yellow").bold().underscore().log(`No results\n`);
     } else if (response.error?.name) {
